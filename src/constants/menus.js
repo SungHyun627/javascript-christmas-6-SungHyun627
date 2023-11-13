dc9045c3ba999ac8a0fbe3774a094450a47f3a31
Sunghyun627 <kksscc4@gmail.com>
2023-11-13 21:09:45 +0900

    fix: 메뉴 리스트 객체 key, value 수정

--- src/constants/menus.js
+++ src/constants/menus.js
@@ -6,69 +6,60 @@ export const MENU_TYPES = Object.freeze({
 });
 
 export const MENU_LIST = Object.freeze({
-  MUSHROOM_SOUP: {
+  양송이스프: {
     MENU_TYPE: MENU_TYPES.APPETIZER,
-    MENU_NAME: '양송이스프',
     MENU_PRICE: 6000,
   },
-  TAPAS: {
+  타파스: {
     MENU_TYPE: MENU_TYPES.APPETIZER,
-    MENU_NAME: '타파스',
     MENU_PRICE: 6000,
   },
-  CAESAR_SALAD: {
+  시저샐러드: {
     MENU_TYPE: MENU_TYPES.APPETIZER,
-    MENU_NAME: '시저샐러드',
     MENU_PRICE: 8000,
   },
-  T_BONE_STEAK: {
+  티본스테이크: {
     MENU_TYPE: MENU_TYPES.MAIN_DISH,
-    MENU_NAME: '티본스테이크',
     MENU_PRICE: 55000,
   },
-  BARBEQUED_RIBS: {
+  바비큐립: {
     MENU_TYPE: MENU_TYPES.MAIN_DISH,
-    MENU_NAME: '바비큐립',
     MENU_PRICE: 54000,
   },
-  SEAFOOD_PASTA: {
+  해산물파스타: {
     MENU_TYPE: MENU_TYPES.MAIN_DISH,
-    MENU_NAME: '해산물파스타',
     MENU_PRICE: 35000,
   },
-  CHRISTMAS_PASTA: {
+  크리스마스파스타: {
     MENU_TYPE: MENU_TYPES.MAIN_DISH,
-    MENU_NAME: '크리스마스파스타',
     MENU_PRICE: 25000,
   },
-  CHOCOLATE_CAKE: {
+  초코케이크: {
     MENU_TYPE: MENU_TYPES.DESSERT,
-    MENU_NAME: '초코케이크',
     MENU_PRICE: 15000,
   },
-  ICE_CREAM: {
+  아이스크림: {
     MENU_TYPE: MENU_TYPES.DESSERT,
-    MENU_NAME: '아이스크림',
     MENU_PRICE: 5000,
   },
-  COKE_ZERO: {
+  제로콜라: {
     MENU_TYPE: MENU_TYPES.DRINK,
-    MENU_NAME: '제로콜라',
     MENU_PRICE: 3000,
   },
-  RED_WINE: {
+  레드와인: {
     MENU_TYPE: MENU_TYPES.DRINK,
-    MENU_NAME: '레드와인',
     MENU_PRICE: 60000,
   },
-  CHAMPAGNE: {
+  샴페인: {
     MENU_TYPE: MENU_TYPES.DRINK,
-    MENU_NAME: '샴페인',
     MENU_PRICE: 25000,
   },
 });
-export const MENU_NAMES = Object.freeze(
-  Object.keys(MENU_LIST).map((menu) => MENU_LIST[menu].MENU_NAME)
-);
+
+export const MENU_PROPERTIES = Object.freeze({
+  MENU_TYPE: 'MENU_TYPE',
+  MENU_NAME: 'MENU_NAME',
+  MENU_PRICE: 'MENU_PRICE',
+});
 
 export const MIN_ORDERED_MENU_COUNT = 1;
