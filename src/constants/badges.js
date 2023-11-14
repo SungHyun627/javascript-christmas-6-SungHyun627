b2913f7ae0c18aea84bb5b4feee8469f236c5b52
Sunghyun627 <kksscc4@gmail.com>
2023-11-14 21:02:08 +0900

    feat: 총 혜택 금액에 따른 부여할 이벤트 배지를 판별하는 기능 구현

---
+++ src/constants/badges.js
@@ -0,0 +1,11 @@
+export const BADGES = Object.freeze({
+  STAR: '별',
+  TREE: '트리',
+  SANTA: '산타',
+});
+export const MIN_TOTAL_BENEFIT_AMOUNT = {
+  STAR: 5000,
+  TREE: 10000,
+  SANTA: 20000,
+};
+export const NOT_MATCHED_BADGE = null;
---
+++ src/validator/EventBadgeValidator.js
@@ -0,0 +1,21 @@
+import { MIN_TOTAL_BENEFIT_AMOUNT } from '../constants/badges';
+
+class EventBadgeValidator {
+  static isStarBadgeGettable(totalBenefitAmount) {
+    return (
+      totalBenefitAmount >= MIN_TOTAL_BENEFIT_AMOUNT.STAR &&
+      totalBenefitAmount < MIN_TOTAL_BENEFIT_AMOUNT.TREE
+    );
+  }
+  static isTreeBadgeGettable(totalBenefitAmount) {
+    return (
+      totalBenefitAmount >= MIN_TOTAL_BENEFIT_AMOUNT.TREE &&
+      totalBenefitAmount < MIN_TOTAL_BENEFIT_AMOUNT.SANTA
+    );
+  }
+  static isSantaBadgeGettable(totalBenefitAmount) {
+    return totalBenefitAmount >= MIN_TOTAL_BENEFIT_AMOUNT.SANTA;
+  }
+}
+
+export default EventBadgeValidator;
